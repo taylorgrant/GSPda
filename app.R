0e1d1f6cff52683254fb20167767a0cb31aebb68
taylorgrant <taylorgrant38@gmail.com>
2025-10-14 10:35:26 -0700

    new modules

--- app.R
+++ app.R
@@ -3,6 +3,8 @@ pacman::p_load(shiny, highcharter, bslib)
 
 source("modules/mod_upload.R")
 source("modules/mod_cor.R")
+source("modules/mod_reg.R")
+source("modules/mod_eda.R")
 
 ui <- bslib::page_sidebar(
   theme = bs_theme(), # optional theming
@@ -14,13 +16,17 @@ ui <- bslib::page_sidebar(
     width = 300
   ),
   tabsetPanel(
-    tabPanel("Correlation", mod_cor_ui("c1"))
+    tabPanel("EDA", mod_eda_ui("eda1")),
+    tabPanel("Correlation", mod_cor_ui("c1")),
+    tabPanel("Regression", mod_reg_ui("lm1"))
   )
 )
 
 server <- function(input, output, session) {
   d <- mod_upload_server("u1")
+  mod_eda_server("eda1", d)
   mod_cor_server("c1", d)
+  mod_reg_server("lm1", d)
 }
 
 shiny::shinyApp(ui, server)
